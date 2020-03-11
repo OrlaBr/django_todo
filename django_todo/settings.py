45ddec7b900a3c4c76becad2ef4bfac086cfb7bf
OrlaBr <orlabreslin@yahoo.com>
2020-03-11 09:44:27 +0000

    Edit settings.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -1,21 +1,6 @@
 import os
 import dj_database_url
-from os import path
-if path.exists("env.py"):
-    import env
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'od5u@bvm(v&x32md6)pw2(by7-eiw@mz-o+!w^3^le(yxg@(lk'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = ['https://django-todo-project.herokuapp.com/', 'localhost']
 
@@ -63,21 +48,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'django_todo.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-#DATABASES = {
- #   'default': {
-  #      'ENGINE': 'django.db.backends.sqlite3',
-   #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-   # }
-#}
-
 DATABASES = {'default': dj_database_url.parse("postgres://pqfhsxuxptlhwg:16a42e8ebff768aa268e9b1fd7cd989dfc595d7f9eb725fc57a1d153e55936d3@ec2-54-75-231-215.eu-west-1.compute.amazonaws.com:5432/d2vqcb8p202pd6")}
 
-
-# Password validation
-# https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -95,9 +67,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/1.11/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -108,8 +77,5 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
